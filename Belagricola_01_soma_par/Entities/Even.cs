f4e8db352463e5fb9f4b869c2ebdfef3870b1fa5
Guilherme Crestani da Silva <gcrestanis@gmail.com>
2022-04-01 13:07:10 -0300

    questao 1 finalizada

---
+++ Belagricola_01_soma_par/Entities/Even.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Belagricola_01_soma_par.Entities
+{
+    class Even
+    {
+        public List<int> Numbers { get; set; }
+
+        public Even(List<int> num)
+        {
+            Numbers = FilterEvenNumbers(num);
+        }
+        private List<int> FilterEvenNumbers(List<int> numbers)
+        {
+            List<int> evens = new List<int>();
+            foreach (int n in numbers)
+            {
+                if (n % 2 == 0)
+                {
+                    evens.Add(n);
+                }
+            }
+            return evens;
+        }
+
+        public double MinSumEvenNumbers()
+        {
+            int min = int.MaxValue;
+            for (int i = 0; i < this.Numbers.Count; i++)
+            {
+                for (int j = 0; j < this.Numbers.Count; j++)
+                {                    
+                    if (j<=i)
+                    {
+                        j = i+1; //avoid that a number is summed with itself
+                        if (j >= this.Numbers.Count)
+                        {
+                            break; //avoid that the last number is summed with itself
+                        }
+                    }
+                    int sum = this.Numbers[i] + this.Numbers[j];
+                    min = sum < min ? sum : min;
+                }
+            }
+            return min;
+        }
+    }
+}
---
+++ Belagricola_01_soma_par/Program.cs
@@ -0,0 +1,28 @@
+﻿using Belagricola_01_soma_par.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Belagricola_01_soma_par
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("QUESTÃO 01: Escreva um algoritmo que some números pares.\n" +
+            "EXPLICAÇÃO: Dada uma lista de números distintos(a sua escolha) encontre a soma dos números\n" +
+            "pares que resulte no menor valor, e deve retornar este valor.\n");
+
+            Console.WriteLine("Considerando a seguinte lista:");
+            Console.WriteLine("1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12");
+
+            List<int> numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };           
+
+            Even evens = new Even(numbers);
+
+            double min = evens.MinSumEvenNumbers();
+            Console.WriteLine("A menor soma entre os números pares acima é: " + min.ToString());
+            Console.ReadLine();
+        }
+    }
+}
