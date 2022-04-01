8e79fbfd435936dabf2fd6f4b5a799ff33f871da
Guilherme Crestani da Silva <gcrestanis@gmail.com>
2022-04-01 14:54:34 -0300

    questao 3 finalizada

---
+++ Belagricola_03_caixa_eletronico/Entities/CaixaEletronico.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Belagricola_03_caixa_eletronico.Entities
+{
+    class CaixaEletronico
+    {
+        static readonly int[] Banknotes = { 100, 50, 20, 10, 5, 2 };
+
+        static public string Withdraw(int value)
+        {
+            int[] qtyBanknotes = { 0, 0, 0, 0, 0, 0 }; //representing notes of 100, 50, 20, 10, 5, 2
+            for (int i = 0; i < Banknotes.Length; i++)
+            {
+                if (value>=Banknotes[i])
+                {
+                    qtyBanknotes[i] = (value / Banknotes[i]);
+                    value = value % Banknotes[i];
+                }
+            }
+            
+            if (value > 0)
+            {
+                return "Erro: o valor a ser sacado deve ser divisível por 2 ou por 5.";
+            }
+
+            string notes = "";
+            for (int i = 0; i < qtyBanknotes.Length; i++)
+            {
+                if (qtyBanknotes[i]>0)
+                {
+                    notes += qtyBanknotes[i].ToString("F0") + " nota(s) de R$" + Banknotes[i].ToString("F2") + "\n";
+                }
+            }
+
+            if (notes !="")
+            {
+                return "\nSaque realizado com sucesso.\n" + notes;
+            }
+            else
+            {
+                return "Erro. Valor mínimo para saque: R$" + Banknotes.Min().ToString("F2");
+            }
+        }
+    }
+}
---
+++ Belagricola_03_caixa_eletronico/Program.cs
@@ -0,0 +1,36 @@
+﻿using Belagricola_03_caixa_eletronico.Entities;
+using System;
+
+namespace Belagricola_03_caixa_eletronico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("QUESTÃO 03: Escreva um algoritmo que simule o funcionamento de um caixa eletrônico.\n"+
+                "EXPLICAÇÃO: Deverá receber o valor desejado de saque e ele retornará à quantidade de cédulas de cada valor.\n"+
+                "As cédulas consideradas pelo caixa eletrônico são: 100, 50, 20, 10, 5 e 2.\n"+
+                "Deve ser retornado a menor quantidade de cédulas possível para o valor do saque.\n");
+
+            Console.WriteLine("Por favor, digite o valor do saque:");
+
+            try
+            {
+                int value = int.Parse(Console.ReadLine());
+                if (value > 0)
+                {
+                    Console.WriteLine(CaixaEletronico.Withdraw(value));
+                }
+                else
+                {
+                    throw new Exception();
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Por favor verifique o valor inserido. Apenas valores inteiros e positivos são aceitos.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
