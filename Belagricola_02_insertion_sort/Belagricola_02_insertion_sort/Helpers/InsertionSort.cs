a0f752ffa7c9ef1b2a44776063ce2b2a37c436f3
Guilherme Crestani da Silva <gcrestanis@gmail.com>
2022-04-01 13:46:30 -0300

    questao 2 finalizada

---
+++ Belagricola_02_insertion_sort/Belagricola_02_insertion_sort/Helpers/InsertionSort.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Belagricola_02_insertion_sort.Helpers
+{
+    static class InsertionSort
+    {
+        static public int[] Sort(int[] inputArray)
+        {
+            for (int i = 0; i < inputArray.Length - 1; i++)
+            {
+                for (int j = i + 1; j > 0; j--)
+                {
+                    if (inputArray[j - 1] > inputArray[j])
+                    {
+                        int temp = inputArray[j - 1];
+                        inputArray[j - 1] = inputArray[j];
+                        inputArray[j] = temp;
+                    }
+                }
+            }
+            return inputArray;
+        }
+    }
+}
---
+++ Belagricola_02_insertion_sort/Belagricola_02_insertion_sort/Program.cs
@@ -0,0 +1,28 @@
+﻿using Belagricola_02_insertion_sort.Helpers;
+using System;
+using System.Collections.Generic;
+
+namespace Belagricola_02_insertion_sort
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("QUESTÃO 02: Escreva um algoritmo de ordenação\n" +
+            "EXPLICAÇÃO: Data uma lista de números distintos(a sua escolha),\n" + "" +
+            "ordene estes números utilizando o método Insertion sort\n");
+
+            Console.WriteLine("Considerando a seguinte lista:");
+            Console.WriteLine("27, 5, 48, 12, 3, 125, 35, 1, -5, -992, -12, 0, 45, 100\n");
+
+            int[] numbers = new int[] { 27, 5, 48, 12, 3, 125, 35, 1, -5, -992, -12, 0, 45, 100 };
+
+            numbers = InsertionSort.Sort(numbers);
+
+            Console.WriteLine("A lista ordenada é:");
+            Console.WriteLine(string.Join(", ", numbers));
+            Console.ReadLine();
+        }
+    }
+}
